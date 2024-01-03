ec538d91be70ac53ead6b901ea11db457c5eac9a
reaperbots <148609775+reaperbots@users.noreply.github.com>
2024-01-03 21:31:22 +0530

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -212,10 +212,11 @@ def get_readable_message():
         buttons.ibutton("Next", "status nex")
         button = buttons.build_menu(3)
     msg += f"<b>☣️ Tasks</b>: {tasks}{bmax_task}"
-    msg += f"\n<b>☣️ Bot Uptime</b>: {currentTime}"
-    msg += f"\n<b>☣️ Free Disk Space</b>: {get_readable_file_size(disk_usage('/usr/src/app/downloads/').free)}"
-    msg += f"\n<b>☣️ Uploading Speed</b>: {get_readable_file_size(up_speed)}/s"
-    msg += f"\n<b>☣️ Downloading Speed</b>: {get_readable_file_size(dl_speed)}/s"
+    msg += f"\n<b>☣️ UPTM</b>: {currentTime}"
+    msg += f"<b> DL</b>: {get_readable_file_size(dl_speed)}/s"
+    msg += f"\n<b>☣️ Free</b>: {get_readable_file_size(disk_usage('/usr/src/app/downloads/').free)}"
+    msg += f"<b> UL</b>: {get_readable_file_size(up_speed)}/s"
+    
     return msg, button
 
 
